4b7bcfaef47dbf88fce6a19a8eafb63f9b88b67e
imadlmao <imadimadblt@gmail.com>
2024-02-14 09:16:17 +0100

    step-1

---
+++ lib/button_values.dart
@@ -0,0 +1,46 @@
+class Buttons {
+  static const String del = 'DEL';
+  static const String clr = 'AC';
+  static const String mul = '×';
+  static const String div = '÷';
+  static const String plu = '+';
+  static const String min = '-';
+  static const String ans = 'Ans';
+  static const String equ = '=';
+  static const String poi = '.';
+  static const String xten = 'x10^(x)';
+
+  static const String n0 = '0';
+  static const String n1 = '1';
+  static const String n2 = '2';
+  static const String n3 = '3';
+  static const String n4 = '4';
+  static const String n5 = '5';
+  static const String n6 = '6';
+  static const String n7 = '7';
+  static const String n8 = '8';
+  static const String n9 = '9';
+
+  static const List<String> buttonValues = [
+    n7,
+    n8,
+    n9,
+    del,
+    clr,
+    n4,
+    n5,
+    n6,
+    mul,
+    div,
+    n1,
+    n2,
+    n3,
+    plu,
+    min,
+    n0,
+    poi,
+    xten,
+    ans,
+    equ
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_calculator/home_page_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,47 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const HomePageView(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Calculator',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 33, 32, 32),
-        ),
-        body: const Column(
-          children: [
-            SizedBox(
-              height: 100,
-            ),
-            TextField(
-              autofocus: true,
-              showCursor: true,
-              keyboardType: TextInputType.none,
-            ),
-          ],
-        ));
   }
 }
